413110161cf4f82f66075292eb080c6aade68afa
Patel_Mayur <patelmayuraaa@gmail.com>
2023-12-20 00:12:39 +0530

    V.003 Added new method DisplayContact

--- src/AdressBookSystem/AdressBook.java
+++ src/AdressBookSystem/AdressBook.java
@@ -32,11 +32,17 @@ public class AdressBook {
             }
         }
     }
+    public void deletePerson(String FirstNameToDelete){
+        int n = items.removeIf(items.getFirst().getFirstName().equals(FirstNameToDelete));
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to AddressBook Program");
         PersonalDetails p = new PersonalDetails("Mayur","Patel","Olpad","Surat","Gujrat","123","00123456789","patelmayuraaa@gmail.com");
+        PersonalDetails p1 = new PersonalDetails("Patel","Mayur"," "," "," ","098","09110911091","patelmayuraaa@gmail.com2");
         AdressBook B =new AdressBook();
         B.addNewContact(p);
+        B.addNewContact(p1);
         B.displayAddedContact();
+        B.deletePerson("Patel");
     }
 }
